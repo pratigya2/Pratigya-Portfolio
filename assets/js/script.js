ea047c04df7d157c7861bf02df7e3642f130052b
pratigya2 <pratigyapaudel0@gmail.com>
2024-02-28 23:12:30 +0545

    new commit

---
+++ assets/js/script.js
@@ -0,0 +1,207 @@
+'use strict';
+
+
+
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+function toggleDetails(btn) {
+    var projectDetails = btn.parentNode.nextElementSibling.querySelector('.project-detail');
+    projectDetails.style.display = projectDetails.style.display === "none" ? "block" : "none";
+}
+
+/**
+ * Mobile navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+addEventOnElements(navTogglers, "click", function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+});
+
+addEventOnElements(navLinks, "click", function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("nav-active");
+});
+var modal = document.getElementById("hire-me-modal");
+
+// Get the link that opens the modal
+var link = document.getElementById("hire-me-link");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the link, open the modal 
+link.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Form submission handling
+document.getElementById("hire-me-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  // You can perform form validation here
+  // Handle form submission (e.g., send AJAX request)
+  // Display a confirmation message to the user
+  var formData = new FormData(this);
+
+      // Construct email body
+      var emailBody = "Email: " + formData.get('email') + "\n\n" + "Message:\n" + formData.get('message');
+
+      // Send email using mailto link
+      window.location.href = "mailto:pratigyapaudel0@gmail.com?subject=Contact%20Form&body=" + encodeURIComponent(emailBody);
+  alert("Your message has been submitted!");
+  // Close the modal
+  modal.style.display = "none";
+});
+
+
+/**
+ * Header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+});
+
+
+
+/**
+ * Element tilt effect
+ */
+
+const tiltElements = document.querySelectorAll("[data-tilt]");
+
+const initTilt = function (event) {
+
+  /** get tilt element center position */
+  const centerX = this.offsetWidth / 2;
+  const centerY = this.offsetHeight / 2;
+
+  const tiltPosY = ((event.offsetX - centerX) / centerX) * 10;
+  const tiltPosX = ((event.offsetY - centerY) / centerY) * 10;
+
+  this.style.transform = `perspective(1000px) rotateX(${tiltPosX}deg) rotateY(${tiltPosY - (tiltPosY * 2)}deg)`;
+
+}
+
+addEventOnElements(tiltElements, "mousemove", initTilt);
+
+addEventOnElements(tiltElements, "mouseout", function () {
+  this.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
+});
+
+
+
+/**
+ * Tab content
+ */
+
+const tabBtns = document.querySelectorAll("[data-tab-btn]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+let lastActiveTabBtn = tabBtns[0];
+let lastActiveTabContent = tabContents[0];
+
+const filterContent = function () {
+
+  if (!(lastActiveTabBtn === this)) {
+
+    lastActiveTabBtn.classList.remove("active");
+    lastActiveTabContent.classList.remove("active");
+
+    this.classList.add("active");
+    lastActiveTabBtn = this;
+
+    const currentTabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
+
+    currentTabContent.classList.add("active");
+    lastActiveTabContent = currentTabContent;
+
+  }
+
+}
+
+addEventOnElements(tabBtns, "click", filterContent);
+
+
+
+/**
+ * Custom cursor
+ */
+
+const cursors = document.querySelectorAll("[data-cursor]");
+const hoveredElements = [...document.querySelectorAll("button"), ...document.querySelectorAll("a")];
+
+window.addEventListener("mousemove", function (event) {
+
+  const posX = event.clientX;
+  const posY = event.clientY;
+
+  /** cursor dot position */
+  cursors[0].style.left = `${posX}px`;
+  cursors[0].style.top = `${posY}px`;
+
+  /** cursor outline position */
+  setTimeout(function () {
+    cursors[1].style.left = `${posX}px`;
+    cursors[1].style.top = `${posY}px`;
+  }, 80);
+
+});
+
+/** add hovered class when mouseover on hoverElements */
+addEventOnElements(hoveredElements, "mouseover", function () {
+  for (let i = 0, len = cursors.length; i < len; i++) {
+    cursors[i].classList.add("hovered");
+  }
+});
+
+/** remove hovered class when mouseout on hoverElements */
+addEventOnElements(hoveredElements, "mouseout", function () {
+  for (let i = 0, len = cursors.length; i < len; i++) {
+    cursors[i].classList.remove("hovered");
+  }
+});
